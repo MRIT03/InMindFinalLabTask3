1af5a69d24023a14544e0a98f0f2be6af032f31c
Riad Terek <Riadot03@gmail.com>
2025-03-13 19:53:42 +0200

    added a bunch of unrevisioned files

---
+++ InMindLab1/Program.cs
@@ -0,0 +1,46 @@
+using InMindLab1.Filters;
+using InMindLab1.Middleware;
+using InMindLab1.Services;
+using InMindLab1.Services.StudentServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IStudentService, StudentService>();
+builder.Services.AddSingleton<RequestLoggingMiddleware>();
+builder.Services.AddScoped<ILoggerActionFilter>();
+builder.Services.AddControllers(options =>
+{
+options.Filters.Add<LoggingActionFilter>();
+});
+
+builder.Services.AddScoped<LoggingActionFilter>();
+builder.Services.AddSingleton<ObjectMapperService>();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>(); 
+builder.Services.AddProblemDetails();
+builder.Services.AddSingleton<ILibraryService, LibraryService>();
+
+var app = builder.Build();
+
+
+app.UseStaticFiles();
+if (app.Environment.IsDevelopment())
+{
+
+}
+app.UseExceptionHandler();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+app.UseMiddleware<RequestLoggingMiddleware>();
+
+app.Run();
---
+++ InMindLab1/Services/ObjectMapperService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+/* This is an object mapper between two types, a source type (Tsource) and a destination type (TDestination)
+ *
+ * The mapper works by first creating a new instance of the destination type
+ *
+ * It then collects all the properties of the source type and destination type
+ *
+ * Then for each property within the source and the destination, if the properties name match (case-insensitive)
+ * then that property in the destination object will be set to the value inside the source object.
+ * this will also check if the property inside the destination object is null/writeable as to avoid any potential errors
+ * properties that do not match between source and destination will be set to null.
+ *
+ * This service highlights the power of reflection and generics as the mapper does not need to know the type of the
+ * source and destination objects at compile time. It will deduce the types at runtime, enhancing its capability in
+ * mapping different types.
+ *
+ * There is an imposed constraint on this mapper, as the TDestination type needs to have a parameterless constructor
+ * 
+ */
+
+
+
+namespace InMindLab1.Services
+{
+    public class ObjectMapperService
+    {
+        // Generics are used here so we don't need to hardcode the types.
+        public TDestination Map<TSource, TDestination>(TSource source) where TDestination : new()
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            TDestination destination = new TDestination();
+
+            // Reflection is used here to determine the type of the objects and allows us to read their properties.
+            PropertyInfo[] sourceProperties = typeof(TSource).GetProperties();
+            PropertyInfo[] destinationProperties = typeof(TDestination).GetProperties();
+
+            foreach (var sourceProp in sourceProperties)
+            {
+                var destProp = destinationProperties.FirstOrDefault(p =>
+                    p.Name.Equals(sourceProp.Name, StringComparison.OrdinalIgnoreCase) &&
+                    p.PropertyType == sourceProp.PropertyType);
+
+                if (destProp != null && destProp.CanWrite)
+                {
+                    destProp.SetValue(destination, sourceProp.GetValue(source));
+                }
+            }
+
+            return destination;
+        }
+    }
+}
